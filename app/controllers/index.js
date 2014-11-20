0f14d0715ca42f945b19f10d0ede5fd060e9667d
Claus Stilborg <cb@nebulators.com>
2014-11-20 10:50:56 +0100

    Data returned to parent through direct access via a Global var

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,2 +1,14 @@
+/***************** EVENT HANDLERS *****************/
+function doClickAsk(e){
+	var args = {
+		question: $.tfQuestion.value
+	};
+	
+	
+	
+	var newwin = Alloy.createController("input",args).getView();
+	newwin.open();	
+}
 
 $.index.open();
+Alloy.Globals.lblAnswer = $.lblAnswer;
--- app/controllers/input.js
+++ app/controllers/input.js
@@ -1,1 +1,15 @@
 var args = arguments[0] || {};
+Ti.API.info("The question is: "+ args.question);
+
+$.lblQuestion.text = args.question;
+//$.tfAnswer.focus();
+
+
+/*************** EVENT HANDLERS ******************/
+function doClickAnswer(e){
+	//Ti.API.info("Alloy.Globals.lblAnswer: " + Alloy.Globals.lblAnswer);
+	//Ti.API.info("Alloy.Globals.lblAnswer apiname: " + Alloy.Globals.lblAnswer.getApiName());
+	//Ti.API.info();
+	Alloy.Globals.lblAnswer.text = $.tfAnswer.value;
+	$.input.close();	
+}
